902a97ba81f77c0cc12639cbbe7dd84e47957090
alpha6926 <alpha6926@gmail.com>
2015-12-08 13:23:52 +0100

    Implémentation des fonctions de DaoProducteur

--- src/fr/esisar/panier/dao/DaoProducteur.java
+++ src/fr/esisar/panier/dao/DaoProducteur.java
@@ -1,39 +1,203 @@
 package fr.esisar.panier.dao;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 
+import fr.esisar.panier.connexionBDD.Connexion;
 import fr.esisar.panier.metier.Producteur;
 
 public class DaoProducteur implements LoDaoPersonne<Producteur>, LoDao<Producteur> {
 
 	@Override
 	public List<Producteur> find(String conditions) {
-		// TODO Auto-generated method stub
-		return null;
+		/*
+		 * SELECT producteur.mail, nom, prenom FROM Producteur, Personne 
+		 * WHERE producteur.mail=personne.mail
+		 * AND (conditions);
+		 */
+		
+		String requete = " SELECT producteur.mail, nom, prenom "
+				+ "FROM Producteur, Personne"
+				+ "WHERE producteur.mail=personne.mail"
+				+ "AND "
+				+ conditions
+				+ ";";
+		
+		// Envoyer la requête
+		Connection c1 = Connexion.getConnection();
+		ResultSet resultats = null;
+		try {
+			Statement stmt = c1.createStatement();
+			resultats = stmt.executeQuery(requete);
+			c1.close();
+		}
+		catch(SQLException e){
+			System.out.println("Erreur d'exÃ©cution requÃªte");
+		}
+		
+		// Exploiter résultats : créer des objets producteurs pour chaque entrée et le mettre dans une liste
+		List<Producteur> liste;
+		try {
+			ResultSetMetaData rsmd = resultats.getMetaData();
+			int nbCols = rsmd.getColumnCount();
+			boolean encore = resultats.next();
+			while (encore) {
+				Producteur p = new Producteur(resultats.getString(1));
+				liste.add(p);
+				encore = resultats.next();
+			}
+			resultats.close();
+			} 
+		catch (SQLException e) {
+			System.out.println("Erreur d'affichage de la requÃªte");
+			}
+		
+		// Renvoyer List<Producteur>
+		return liste;
 	}
 
 	@Override
 	public boolean create(Producteur newRecord) {
-		// TODO Auto-generated method stub
+		char bool;
+		if(newRecord.isPaid()){
+			bool='t';
+		}
+		else{
+			bool='f';
+		}
+		
+		/* INSERT INTO Personne VALUES (Producteur.getMail(), Producteur.getPrenom(), Producteur.getNom());
+		 * INSERT INTO Producteur VALUES (Producteur.getMail(),Producteur.getOrdreCheque(), char);
+		*/
+		
+		String requete_personne = "INSERT INTO Personne VALUES ('"
+				+ newRecord.getMail()
+				+"','"
+				+ newRecord.getPrenom()
+				+ "','"
+				+ newRecord.getNom()
+				+ "');";
+		
+		String requete_producteur = "INSERT INTO Personne VALUES ('"
+				+ newRecord.getMail()
+				+"','"
+				+ newRecord.getOrdreCheque()
+				+ "','"
+				+ bool
+				+ "');";
+		
+		//Envoyer les requêtes
+		Connection c1 = Connexion.getConnection();
+		ResultSet resultats = null;
+		try {
+			Statement stmt = c1.createStatement();
+			resultats = stmt.executeQuery(requete_personne);
+			c1.close();
+		}
+		catch(SQLException e){
+			System.out.println("Erreur d'exÃ©cution requÃªte");
+		}
+		
+		c1 = Connexion.getConnection();
+		resultats = null;
+		try {
+			Statement stmt = c1.createStatement();
+			resultats = stmt.executeQuery(requete_personne);
+			c1.close();
+		}
+		catch(SQLException e){
+			System.out.println("Erreur d'exÃ©cution requÃªte");
+		}
+
 		return false;
 	}
 
 	@Override
 	public boolean update(Producteur updateRecord) {
-		// TODO Auto-generated method stub
+		/* UPDATE Producteur SET ordreCheque=Producteur.getOrdreCheque()
+		 * WHERE Producteur.mail=Producteur.getMail();
+		 */
+		
+		String requete = "UPDATE Producteur SET ordreCheque='"
+				+ updateRecord.getOrdreCheque()
+				+ "' WHERE Producteur.mail='"
+				+ updateRecord.getMail()
+				+ "';";
+		
+		
+		// Envoyer la requête
+		Connection c1 = Connexion.getConnection();
+		ResultSet resultats = null;
+		try {
+			Statement stmt = c1.createStatement();
+			resultats = stmt.executeQuery(requete);
+			c1.close();
+		}
+		catch(SQLException e){
+			System.out.println("Erreur d'exÃ©cution requÃªte");
+		}
+		
 		return false;
 	}
 
 	@Override
 	public boolean remove(Producteur removeRecord) {
-		// TODO Auto-generated method stub
+		/*
+		 * DELETE FROM Producteur WHERE Producteur.mail=Producteur.getMail();
+		 */
+		
+		String requete = "DELETE FROM Producteur WHERE Producteur.mail='"
+				+ removeRecord.getMail()
+				+ "';";
+				
+		
+		//Envoyer la requête
+		Connection c1 = Connexion.getConnection();
+		ResultSet resultats = null;
+		try {
+			Statement stmt = c1.createStatement();
+			resultats = stmt.executeQuery(requete);
+			c1.close();
+		}
+		catch(SQLException e){
+			System.out.println("Erreur d'exÃ©cution requÃªte");
+		}
+
 		return false;
 	}
 
 	@Override
 	public Producteur getByMail(String mail) {
-		// TODO Auto-generated method stub
-		return null;
+		List<Producteur> liste;
+		liste=find("Producteur.mail='"+mail+"'");
+		
+		// Envoyer la requête -> find le fait
+		// Exploiter résultat -> find le fait
+		// Renvoyer List<Producteur>
+		return liste.get(1);
+	}
+	
+	
+	public Producteur getByPaid(boolean paid) {
+		char bool;
+		if(paid){
+			bool='t';
+		}
+		else{
+			bool='f';
+		}
+		List<Producteur> liste;
+		liste=find("Producteur.paiementProdBool='"+bool+"'");
+		
+		// Envoyer la requête -> find le fait
+		// Exploiter résultat -> find le fait
+		// Renvoyer List<Producteur>
+		return liste.get(1);
+		
 	}
 
 }
